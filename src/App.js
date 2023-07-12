30d90e830c7e0bc9c8b48b0f098605e932d4e8ab
Ithrendrial <rcox2990@gmail.com>
2023-07-12 23:12:34 +1200

    Test changes on app.js

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          The beginnings of Chaos App.
+          The beginnings of the Chaos App.
         </p>
       </header>
     </div>
