12637bab4b864f205dc9ece20523a6160bc7f1b7
Ithrendrial <rcox2990@gmail.com>
2023-07-12 23:34:52 +1200

    Test changes on app.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import logo from './logo.svg';
+import ditto from './ditto.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={ditto} className="App-logo" alt="logo" />
         <p>
           The beginnings of the Chaos App.
         </p>
