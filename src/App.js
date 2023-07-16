8370cd9cd5d520b1921c128fe821987bc0f315e1
jWaitoa <jackwaitoa@gmail.com>
2023-07-16 21:10:06 +1200

    fixed css

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ function App() {
         <img src={ditto} className="App-logo" alt="logo" />
           <h1>Kia Rite</h1>
           <Button /*press={joinGame}*/ name="Join Game"/>
+          <br />
           <Button /*press={hostGame}*/ name="Host Game"/>
         <p>
           The beginnings of the Chaos App.
