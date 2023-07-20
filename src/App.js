f414de121cf70c613412ab0c18a6f5d9710d26df
Rachel <83641601+Ithrendrial@users.noreply.github.com>
2023-07-20 22:59:29 +1200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,11 +6,12 @@ import Input from './Input.js'
 function App() {
   return (
     <div className="App">
-          <h1>Kia Rite</h1>
-          <div className="button_container">
-              <Input name="Enter Name"/>
-              <Button /*press={joinGame}*/ name="Join Session"/>
-          </div>
+            <h1>Kia Rite</h1>
+            <h2>GET STARTED</h2>
+            <div className="button_container">
+                <Input name="Enter Nickname"/>
+                <Button /*press={joinGame}*/ name="NEXT"/>
+            </div>
     </div>
   );
 }
