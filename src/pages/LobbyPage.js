8ead04712c418ce7445bb1063d500e8f2c80a0c2
jWaitoa <jackwaitoa@gmail.com>
2023-07-21 02:05:23 +1200

    Added Lobbypage

--- src/pages/LobbyPage.js
+++ src/pages/LobbyPage.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import '../App.css';
-import Button from '../Button.js'
-import Input from '../Input.js'
+import style from '../styles/LobbyPage.module.css';
+import Button from '../Button.js';
 
-const Homepage = () => {
-    return (
-        <div className="App">
-            <h1>Kia Rite</h1>
-            <p>this is the lobby</p>
-        </div>
+const LobbyPage = () => {
+    return (    
+        <>
+            <h1 className={style.h1}>Kia Rite</h1>
+            <h2 className={style.h2}>
+                GAME PIN: <br />
+                4845
+            </h2>
+            <div className={style.player_container}>
+                    <p>Player 1</p>
+                    <p>Player 2</p>
+                    <p>Player 3</p>
+                    <p>Player 4</p>
+                    <p>Player 5</p>
+                    <p>Player 6</p>
+                    <p>Player 7</p>
+                    <p>Player 8</p>
+            </div>
+            <div className={style.button_container}>
+                <Button name="INVITE" />
+                <Button name="PLAY" />
+            </div>     
+        </>
     );
 }
-
-export default Homepage;
+export default LobbyPage;
