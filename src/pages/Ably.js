cc06700d3d0da1fca9442a2f95b2c9041ee04bda
jWaitoa <jackwaitoa@gmail.com>
2023-08-01 16:09:46 +1200

    perf: Reduced a large amount of presence event messages being published
    Work is required to reduce presence event messages further

--- src/pages/Ably.js
+++ src/pages/Ably.js
@@ -1,3 +1,12 @@
+
+/*    
+Bugged code:
+    updateStatus("new status");
+
+    // Convert presence data to list items in render.
+    const members = presenceData.map((msg, index) => <li key={index}>{msg.clientId}:{msg.data}</li>);
+}
+*/
 import React, { useState } from 'react';
 import { configureAbly, useChannel, usePresence } from "@ably-labs/react-hooks";
 import '../App.css';
@@ -5,36 +14,39 @@ import '../App.css';
 import Button from '../components/Button.js'
 import Input from '../components/Input.js'
 
-configureAbly({ key: "yqb0VQ.Av_Gmg:pItSDLVHuUqgEGYCqdOhVSr4Ypktm7764_a0mhpwbEY",  clientId: generateRandomId() });
+configureAbly({ key: "yqb0VQ.Av_Gmg:pItSDLVHuUqgEGYCqdOhVSr4Ypktm7764_a0mhpwbEY", clientId: generateRandomId() });
 
-const Ably = () => {    
-    const [messages, updateMessages] = useState([]);
-    const [channel] = useChannel("lobby", (message) => { // useChannel call returns an instance of a channel
+function Ably() {
+     const [messages, updateMessages] = useState([]);
+     const [channel] = useChannel("your-channel-name", (message) => {
         updateMessages((prev) => [...prev, message]);
-    });   
-    
-    const [presenceData, updateStatus] = usePresence("lobby"/*, (update) => {
-        console.log(update);
-      }*/);
+    });
 
-    updateStatus("new status");
-
-    // Convert presence data to list items in render.
-    const members = presenceData.map((msg, index) => <li key={index}>{msg.clientId}:{msg.data}</li>);
-    
     const [nickname, setNickname] = useState("");
 
-    const messagePreviews = messages.map((msg, index) => <li key={index}>{msg.data.text}</li>);
+    const [presenceData] = usePresence("your-channel-name", (update) => {
+        console.log(update);
+    });
 
-    return(
+    const messagePreviews = messages.map((msg, index) => <li key={index}>{msg.data.text}</li>);
+    const presentClients = presenceData.map((msg, index) => (
+        <li key={index}>
+            ID: {msg.clientId}  |  Nickname: {nickname}
+        </li>
+    ));
+
+    return (
         <div className="App">
             <div className="title">Chaos App</div>
             <div className="heading">GET STARTED</div>
             <div className="container">
-                <Input placeholder="Enter Nickname" onChange={(e) => setNickname(e.target.value)}/>
-                <Button name="NEXT" press={() => {channel.publish(("message"), { text: nickname })}}/>
+                {<Input placeholder="Enter Nickname" onChange={(e) => setNickname(e.target.value)}/>}
+                {<Button name="NEXT" press={() => {channel.publish(("message"), { text: nickname })}}/>}
             </div>
-            {messagePreviews}
+            <h2>Present Clients</h2>
+            <ul>{presentClients}</ul>
+            <h2>Nicknames</h2>
+            <ul>{messagePreviews}</ul>
         </div>
     );
 }
