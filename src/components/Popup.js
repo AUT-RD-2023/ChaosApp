711a9dace0adb58d8147b02d38fd34a58d3eed6b
Jade Thompson-Tavai <jhp4379@autuni.ac.nz>
2023-08-10 15:22:28 +1200

    Add: Settings available to host only
    - Moved Popup.css to styles
    - Settings popup only available to host

--- src/components/Popup.js
+++ src/components/Popup.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import './Popup.css'
+import style from '../styles/Popup.css';
 
 function Popup(props) {
   return (props.trigger) ? (
     <div className="popup">
         <div className="popup-inner">
            <button className="close-btn" onClick={() => props.setTrigger(false)}>close</button>
-            { props.children }
+                { props.children }
         </div>
     </div>
   ) : ""
