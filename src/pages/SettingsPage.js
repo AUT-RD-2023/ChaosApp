c9218c393171669c4cfde28502a2b5119378db40
Jade Thompson-Tavai <jhp4379@autuni.ac.nz>
2023-08-11 14:37:50 +1200

    Modfy: Setter.css and SettingsPage.js

--- src/pages/SettingsPage.js
+++ src/pages/SettingsPage.js
@@ -1,4 +1,4 @@
-// import './SettingsPage.css';
+import '../components/Setter.css';
 import Setter from '../components/Setter';
 import '../App.css'
 
@@ -6,12 +6,15 @@ function SettingsPage() {
 return (
     <div className="settings">
         <h1 className="title">SETTINGS</h1>
-        <div className="number-of-rounds">
-        <h3 className= "subtitle">Number of rounds</h3>
-        {/* replace {value} with default */}
-        <Setter value="5" />
-        </div>
+        <form>
+            <div className="number-of-rounds">
+                <h3 className= "subtitle">Number of rounds</h3>
+                {/* replace {value} with default */}
+                <Setter value="05" />
+            </div>
+        </form>
     </div>
+    
 
 )
 
