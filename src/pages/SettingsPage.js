8fbd8f7cb955e532f0817b14c33a8716f5698ac1
Jade Thompson-Tavai <jhp4379@autuni.ac.nz>
2023-08-11 13:57:49 +1200

    Modify Popup.css and Setter.css

--- src/pages/SettingsPage.js
+++ src/pages/SettingsPage.js
@@ -1,10 +1,11 @@
-// import './SettingsPage.css'
-import Setter from '../components/Setter'
+// import './SettingsPage.css';
+import Setter from '../components/Setter';
+import '../App.css'
 
 function SettingsPage() {
 return (
     <div className="settings">
-        <h1 className="title">Settings</h1>
+        <h1 className="title">SETTINGS</h1>
         <div className="number-of-rounds">
         <h3 className= "subtitle">Number of rounds</h3>
         {/* replace {value} with default */}
