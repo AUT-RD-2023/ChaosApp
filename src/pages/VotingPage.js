26dbe8cb2e01c071ee38d5bbf534f4c118b56f0b
Jade Thompson-Tavai <jhp4379@autuni.ac.nz>
2023-08-25 01:13:23 +1200

    add basic CSS for voting frontend
    - Add CSS for frontend
    - Remove CSS testing files
    - No functionality yet

--- src/pages/VotingPage.js
+++ src/pages/VotingPage.js
@@ -1,20 +1,65 @@
-import React from 'react';
+/*
+* VotingPage.js
+* Author: Jade Thompson-Tavai
+* Date: 24-08-23
+*
+* Contributors:
+*
+*
+*/
+
+// React
+import React from "react";
+
+// Components
+import Button from "../components/Button.js";
+import TimerBar from "../components/TimerBar.js";
+
+// Styles
+// import styles from "../styles/DiscussionPage.module.css";
+import styles from "../styles/VotingPage.module.css";
+
+import "../App.css";
 
 
 const VotingPage = () => {
 
+    const name = "Name".toUpperCase();
 
-return (
-    <div className="App">
-          <div className="title">Chaos</div>
-            
-            <div className="heading">GET STARTED</div>
 
-            <div className="container">
-
+    return (
+        <div className={styles.page}>
+            <div className={styles.header}>
+                <div className={styles.subheader}>
+                    <div className="name">{name}</div>
+                    <div className="round">1/8 VOTED</div>
+                </div>
+                <TimerBar />
             </div>
-    </div>
-)
+            <div className={styles.container}>
+                <div className={styles.subtitle}>TAKE A VOTE</div>
+            </div>
+        {/* Container for response cards */}
+        
+        <div className={styles.carouselContainer}>
+            <div className={styles.carousel}>
+                {/* Needs to be optimised for functionality - currently placeholders */}
+                <div className={styles.card}>Response 1</div>
+                <div className={styles.card}>Response 2</div>
+                <div className={styles.card}>Response 3</div>
+                <div className={styles.card}>Response 4</div>
+                <div className={styles.card}>Response 5</div>
+                <div className={styles.card}>Response 6</div>
+                <div className={styles.card}>Response 7</div>
+                <div className={styles.card}>Response 8</div>
+            </div>
+        </div>
+        <div className={styles.buttons}>
+            <Button name="VOTE" />
+        </div>
+
+        </div>
+    )
 }
 
 export default VotingPage;
