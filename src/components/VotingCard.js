704dce558261207762e571dc8eb8fe5e5d6e7278
Rob Deles <wwj7794@autuni.ac.nz>
2023-09-22 19:20:06 +1200

    fix: fixed long responses in voting card
    -added and inner div to the voting card.
    -long responses are rendered properly and is scrollable.

--- src/components/VotingCard.js
+++ src/components/VotingCard.js
@@ -1,8 +1,10 @@
 import styles from '../styles/VotingPage.module.css';
 
 const VotingCard = (props) => { 
-    return(        
-        <div className={styles.card} tabIndex={0} onFocus={props.onFocus}>{props.response} <strong>{props.votes}</strong></div>
+    return(    
+        <div className={styles.card}>
+        <div className={styles.inner} tabIndex={0} onFocus={props.onFocus}>{props.response} <strong>{props.votes}</strong></div>
+        </div>
     );
 }
 
