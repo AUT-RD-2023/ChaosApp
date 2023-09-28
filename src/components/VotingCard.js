fd2c0dae78c3e0cab37df752231ccd7a9637fb33
Ithrendrial <rcox2990@gmail.com>
2023-09-29 02:37:28 +1300

    fix: amendment on voting card to fix votng page css

--- src/components/VotingCard.js
+++ src/components/VotingCard.js
@@ -3,7 +3,7 @@ import styles from '../styles/VotingPage.module.css';
 const VotingCard = (props) => { 
     return(    
         <div 
-            className={props.focusable ? styles.card : props.selected ? styles.card3 : styles.card2} 
+            className={styles.card}
             tabIndex={0} 
             onFocus={props.onFocus}>
                 {props.response}&nbsp;<strong>{props.votes}</strong>
